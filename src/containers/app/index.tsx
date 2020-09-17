b8b63708bb36de127d67d27212ba5797f216cd73
Tarun Shukla <tarun_shukla@persistent.co.in>
2020-09-17 20:09:33 +0530

    Initial commit to create folder structure and other dependencies

---
+++ src/containers/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Write your code here!!</header>
+    </div>
+  );
+}
+
+export default App;
